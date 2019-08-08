11ffe976bd502d381149c53294dad5301aa3f67d
flekssk <fks-sk@yandex.ru>
2019-08-09 01:37:15 +0400

    create bundle configuration and inject it into container

---
+++ src/DependencyInjection/CalculatorExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FKS\stringCalculator\DependencyInjection;
+
+use Exception;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class CalculatorExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $configs
+     * @param ContainerBuilder $container
+     *
+     * @throws Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $this->processConfiguration((new Configuration()), $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $loader->load('services.yaml');
+    }
+}
