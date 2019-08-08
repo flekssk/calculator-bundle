536b8a6c3395ac990b26a10934d5006bbdfb4a5c
flekssk <fks-sk@yandex.ru>
2019-08-09 02:06:06 +0400

    define base exceptions

---
+++ src/Exceptions/IllegalCharset.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FKS\stringCalculator\Exceptions;
+
+use Exception;
+
+class IllegalCharset extends Exception
+{
+}
---
+++ src/Exceptions/WrongCharacterSequence.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FKS\stringCalculator\Exceptions;
+
+use Exception;
+
+class WrongCharacterSequence extends Exception
+{
+}
