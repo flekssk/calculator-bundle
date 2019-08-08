9924bd80914a9ef5e3c3b8ff11b648b9506bff73
flekssk <fks-sk@yandex.ru>
2019-08-09 03:02:52 +0400

    result class

---
+++ src/Calculators/Result.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace FKS\stringCalculator\Calculators;
+
+class Result
+{
+    /** @var bool */
+    protected $success = false;
+    /** @var string */
+    protected $error = '';
+    /** @var int  */
+    protected $result = 0;
+
+    /**
+     * @return bool
+     */
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    /**
+     * @param bool $success
+     */
+    public function setSuccess(bool $success): void
+    {
+        $this->success = $success;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResult() : int
+    {
+        return $this->result;
+    }
+
+    /**
+     * @param mixed $result
+     */
+    public function setResult($result): void
+    {
+        $this->result = $result;
+    }
+
+    /**
+     * @return string
+     */
+    public function getError(): string
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param string $error
+     */
+    public function setError(string $error): void
+    {
+        $this->error = $error;
+    }
+}
