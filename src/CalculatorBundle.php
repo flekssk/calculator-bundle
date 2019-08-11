1587848a0228508801af5f6bb4c5fa3011af7231
flekssk <fks-sk@yandex.ru>
2019-08-12 01:18:01 +0400

    remove calculator registration from wrong place

--- src/CalculatorBundle.php
+++ src/CalculatorBundle.php
@@ -4,6 +4,9 @@ namespace FKS\StringCalculator;
 
 use FKS\StringCalculator\Calculators\DefaultCalculator;
 use FKS\StringCalculator\Contacts\Calculator as CalculatorInterface;
+use Symfony\Component\Config\ConfigCache;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -12,7 +15,5 @@ class CalculatorBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-
-        $container->register(CalculatorInterface::class, DefaultCalculator::class);
     }
 }
