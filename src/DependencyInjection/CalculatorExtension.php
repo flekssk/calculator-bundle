94d633a6fb3476b0b739f29cf4a848bc1e6a14f5
flekssk <fks-sk@yandex.ru>
2019-08-12 01:16:21 +0400

    use config to register calculators by aliases

--- src/DependencyInjection/CalculatorExtension.php
+++ src/DependencyInjection/CalculatorExtension.php
@@ -3,6 +3,11 @@
 namespace FKS\StringCalculator\DependencyInjection;
 
 use Exception;
+use FKS\StringCalculator\Contacts\Calculator;
+use FKS\StringCalculator\Contacts\Result;
+use FKS\StringCalculator\Exceptions\CalculatorNotFound;
+use FKS\StringCalculator\Exceptions\ResultClassNotFound;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -20,10 +25,39 @@ class CalculatorExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $this->processConfiguration((new Configuration()), $configs);
-
+        $config = $this->processConfiguration((new Configuration()), $configs);
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load('services.yaml');
+
+
+        $container->addAliases(
+            [
+                Calculator::class => $this->getCalculatorAccessor($config['options']['calculatorAlias']),
+                Result::class     => $this->getResultAccessor($config['options']['resultAlias']),
+            ]
+        );
+    }
+
+    public function getCalculatorAccessor($calculatorAlias)
+    {
+        $class = 'FKS\\StringCalculator\\Calculators\\' . ucfirst($calculatorAlias) . 'Calculator';
+
+        if (!class_exists($class)) {
+            throw new CalculatorNotFound('Calculator ' . $calculatorAlias . ' doesn`t exists');
+        }
+
+        return $class;
+    }
+
+    public function getResultAccessor($resultAlias)
+    {
+        $class = 'FKS\\StringCalculator\\Calculators\\' . ucfirst($resultAlias) . 'Calculator';
+
+        if (!class_exists($class)) {
+            throw new ResultClassNotFound('Calculator ' . $resultAlias . ' doesn`t exists');
+        }
+
+        return $class;
     }
 }
